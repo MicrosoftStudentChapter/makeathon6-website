e72e92e6c125e6eace926dde380dd1d1d5d7b8a4
Mankirat Singh <msingh2_be22@thapar.edu>
2024-01-13 13:56:42 +0530

    change speakers to tracks
    Signed-off-by: Mankirat Singh <msingh2_be22@thapar.edu>

--- app/speakers/page.js
+++
@@ -1,5 +0,0 @@
-import styles from './speakers.module.css'
-
-export default function Page() {
-    return <h1 className={styles.thisH1}>speakers Page!</h1>
-  }
---
+++ app/tracks/page.js
@@ -0,0 +1,5 @@
+import styles from './tracks.module.css'
+
+export default function Page() {
+    return <h1 className={styles.thisH1}>tracks Page!</h1>
+  }
