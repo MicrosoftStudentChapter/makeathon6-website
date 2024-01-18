5e19febcf4faed1b3ae948d5e3693af450291ae0
man0407 <chaturvedimanas15@gmail.com>
2024-01-18 22:32:34 +0530

    first commit

--- app/tracks/page.js
+++ app/tracks/page.js
@@ -1,5 +1,6 @@
 import styles from './tracks.module.css'
 
 export default function Page() {
-    return <h1 className={styles.thisH1}>tracks Page!</h1>
+    return <div><h1 className={styles.thisH1}>tracks Page!</h1></div>
   }
+  
