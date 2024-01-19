b2457b85bd5000c22a3fd86b0c65bb86fe1ebb51
vishnu1345 <vishnu149816@gmail.com>
2024-01-19 17:00:24 +0530

    Co-authored-by: Mehar Walia <MW-5705@users.noreply.github.com> Co-authored-by: 09-ig <09-ig@users.noreply.github.com>

--- app/page.js
+++ app/page.js
@@ -1,16 +1,48 @@
 import Image from 'next/image'
-import img from './images/bg.png'
+// import img from './images/bg.png'
 import styles from './page.module.css'
 import HomeButton from '@/components/button/button'
-
+// import logo from './images/MLSClogo.png'
+// import img from '/'
 export default function Home() {
   return (
     <>
+
+
       <div className={styles.home}>
-        <h1 className={styles.thisH1}>Makeathon 6 </h1>
-        <div className={styles.tilthome}><HomeButton title="ABOUT" description="description" />
+
+        <Image src="/throne.png" height={500} width={500} alt="hello" className={styles.throne}/>
+        {/* <Image src="/MLSClogo.png" width={7000} height={40} className={styles.logo}/> */}
+        
+        <div className={styles.allButtons}>
+{/* 
+          <div className={styles.row1}>
+            <div className={styles.tiltHomeLeft1}><HomeButton title="ABOUT" description="description" />
+            </div>
+            <div className={styles.tiltHomeRight1}><HomeButton title="ABOUT" description="description" /></div>
+          </div> */}
+
+
+        <div className={styles.parallelBtns}>
+
+          <div className={styles.row2}>
+            <div className={styles.tiltHomeLeft2}><HomeButton title="ABOUT" description="description" />
+            </div>
+            <div className={styles.tiltHomeRight2}><HomeButton title="ABOUT" description="description" /></div>
+          </div>
+
+
+
+          <div className={styles.row3}>
+            <div className={styles.tiltHomeLeft3}><HomeButton title="ABOUT" description="description" /></div>
+            <div className={styles.tiltHomeRight3}><HomeButton title="ABOUT" description="description" /></div>
+          </div>
+
         </div>
 
+        </div>
+
+        {/* <div className={styles.logo}><img src='https://drive.google.com/drive/my-drive?q=after:2024-01-17%20parent:0AP5-qxORQB99Uk9PVA'></img></div> */}
       </div>
     </>
   )
--- components/button/button.js
+++ components/button/button.js
@@ -3,14 +3,16 @@ import styles from './button.module.css'
 function HomeButton(props) {
     return ( 
         <>
-        <div>
+        <div className={styles.homeButton}>
         <div className = {styles.top}></div>
         <div className = {styles.home}>
              
             <h3><span>{props.title}</span></h3>
+           
             <div className={styles.box}></div>
             <h4 className={styles.description}><span>{props.description}</span> <img src='https://freesvg.org/img/ninja-blade.png'></img></h4>
         </div>
+
         </div>
         </>
      );
