fc864106c450f55b433a266098d24ee98b68dabc
Jeevant Verma <jverma_be23@thapar.edu>
2024-01-20 14:17:43 +0530

    Removed clickme button and added assets

--- app/tracks/page.js
+++ app/tracks/page.js
@@ -1,3 +1,4 @@
+"use client"
 import styles from './tracks.module.css'
 
 export default function Page() {
@@ -10,19 +11,16 @@ export default function Page() {
        <div className={styles.common}>
         <h1 className={styles.Subtitle}>Event 1</h1>
         <p className={styles.para}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed architecto natus tempora, ex pariatur accusantium rem, ullam delectus cumque, cupiditate id temporibus! Perspiciatis nisi, aut voluptates quidem non nostrum aperiam!</p>
-        <button className={styles.click}>Click Me!</button>
        </div>
         
        <div className={styles.common} >
         <h1 className={styles.Subtitle}>Event 2</h1>
         <p className={styles.para}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quos nisi ad autem enim nam doloribus necessitatibus tempora iste perferendis vero deleniti voluptatem quam, eos ex, minima atque consectetur blanditiis!</p>
-        <button className={styles.click}>Click Me!</button>
        </div>
         
        <div className={styles.common}>
         <h1 className={styles.Subtitle}>Event 3</h1>
         <p className={styles.para}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero deserunt, doloremque tenetur cupiditate exercitationem temporibus tempore eligendi maiores, provident ut quos, facere velit ratione necessitatibus voluptas ullam inventore dolores sit.</p>
-        <button className={styles.click}>Click Me!</button>
        </div>
       </div>  
     </div>
