d878eb8f9b28dbf730b7c263f890531d58f794fe
Yuvansh Pathak <pathakyuvansh1994@gmail.com>
2024-01-24 22:44:13 +0530

    First commit

---
+++ app/timeline/assets/bg.js
@@ -0,0 +1,77 @@
+'use client'
+import React from "react"; 
+import Particles from "react-tsparticles"; 
+import { loadFull } from "tsparticles"; 
+const App = () => { 
+  const particlesInit = async (main) => { 
+    console.log(main); 
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets 
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready 
+    // starting from v2 you can add only the features you need reducing the bundle size 
+    await loadFull(main); 
+  }; 
+  const particlesLoaded = (container) => { 
+    console.log(container); 
+  }; 
+  return ( 
+    <div className="body">
+    <div className="App"> 
+      <Particles 
+        id="tsparticles" 
+        init={particlesInit} 
+        loaded={particlesLoaded} 
+        options={{ 
+          background: { 
+            color: "rgb(10,10,25)", 
+          }, 
+          fpsLimit: 60, 
+          particles: { 
+            shape: { 
+              type: "circle", 
+            }, 
+            size: { 
+              random: { 
+                enable: true, 
+                minimumValue: 0.5, 
+              }, 
+              value: 1.4, 
+            }, 
+            color: { 
+              value: "#f1f1f1", 
+            }, 
+            number: { 
+              density: { 
+                enable: true, 
+                area: 1080, 
+              }, 
+              limit: 0, 
+              value: 800, 
+            }, 
+            opacity: { 
+              animation: { 
+                enable: true, 
+                minimumValue: 0.5, 
+                speed: 1.6, 
+                sync: false, 
+              }, 
+              random: { 
+                enable: true, 
+                minimumValue: 0.1, 
+              }, 
+              value: 1, 
+            }, 
+            interactivity: { 
+              detectsOn: "canvas", 
+              events: { 
+                resize: true, 
+              }, 
+            }, 
+          }, 
+        }} 
+      /> 
+    </div> 
+    </div>
+  ); 
+}  
+export default App;
+
