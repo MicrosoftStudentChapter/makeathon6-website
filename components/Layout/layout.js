5b4421cda4601639cd0e9df3d11e2674c939345a
vishnu1345 <vishnu149816@gmail.com>
2024-02-05 00:24:21 +0530

    fixed loader

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,10 +1,10 @@
 "use client"
-
 import React, { Suspense } from 'react';
 import { useState, useEffect } from 'react';
 import styles from './layout.module.css';
 
 import Crown from '../Crown/crown';
+/*
 
 const Layout = ({ children }) => {
   const [showContent, setShowContent] = useState(false);
@@ -35,6 +35,41 @@ const LoadingFallback = () => (
   );
   
   export default Layout;
+  */
+
+
+ 
+
+const Layout = ({ children }) => {
+  const [showContent, setShowContent] = useState(false);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowContent(true);
+    }, 800); 
+    
+    return () => clearTimeout(timer);
+  }, []);
+  
+  return (
+    <div className={styles.loading}>
+      {showContent ? (
+        <Suspense fallback={<LoadingFallback />}>
+          {children}
+        </Suspense>
+      ) : (
+        <Crown />
+      )}
+    </div>
+  );
+};
+
+const LoadingFallback = () => (
+  <p>Loading...</p>
+);
+
+export default Layout;
+
   // */
   
   // import styles from './layout.module.css';
