c8d3f742691bd05e4591950383c047f6818c581c
Jeevant Verma <jverma_be23@thapar.edu>
2024-02-10 16:39:37 +0530

    Footer responsiveness and Icons upgraded

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,14 +1,14 @@
 import styles from './footer.module.css';
-import King from './assets/King.png';
-import Fin from './assets/FinSec.png';
+import King from './assets/newsec.png';
+import Fin from './assets/newsec.png';
 import Mail from './assets/mail.png';
-import Location from './assets/location.png';
+import Location from './assets/locate.png';
 import LD from './assets/linkedin.png'
 import Yt from './assets/yt.png'
 import Ig from './assets/insta.png'
 import Fb from './assets/fb.png'
 import Logo from './assets/Logo.png'
-import Sr from './assets/Sponsor.png'
+import Sr from './assets/spons.png'
 
 import Image from 'next/image'
 import Link from 'next/link'
@@ -61,19 +61,19 @@ export const Footer = () => {
 
               <a href='mailto: msc@thapar.edu' target='_blank' style={{ textDecoration: 'none'}}>
                 <div className={styles.bleh}>
-                  <Image className={styles.image1} src={Mail}/> <div className={styles.side}>msc@thapar.edu</div>
+                  <Image className={styles.image2} src={Mail}/> <div className={styles.side}>msc@thapar.edu</div>
                 </div>
               </a>
 
               <a href='https://goo.gl/maps/bMSfh8uoYCVuLEjD6' target='_blank' style={{ textDecoration: 'none'}}> 
                 <div className={styles.bleh}>
-                  <Image className={styles.image2} src={Location}/> <div className={styles.side}>TIET Patiala</div>
+                  <Image className={styles.image} src={Location}/> <div className={styles.side}>TIET Patiala</div>
                 </div>
               </a>
 
               <Link href="/Proposal.pdf" passHref style={{ textDecoration: 'none'}}>
                 <div className={styles.bleh}>
-                    <Image className={styles.image3} src={Sr} /> <div className={styles.side}>Sponsor Us!</div>
+                    <Image className={styles.image} src={Sr} /> <div className={styles.side}>Sponsor Us!</div>
                 </div>
               </Link>
 
@@ -83,16 +83,16 @@ export const Footer = () => {
             
             <a href='https://www.linkedin.com/in/apoorva11/' target='_blank' style={{ textDecoration: 'none'}}>
               <div className={styles.bleh}>
-                <Image className={styles.image4} src={King}/> <div className={styles.side}>Apoorva</div><br></br>
+                <Image className={styles.image3} src={King}/> <div className={styles.side}>Apoorva</div><br></br>
               </div>
               <span className={styles.gen}>(General Secretary)</span>
             </a>
 
             <a href='https://www.linkedin.com/in/utkarsh-tripathi-80a0ab192/' target='_blank' style={{ textDecoration: 'none'}}>
               <div className={styles.bleh}>
-                <Image className={styles.image5} src={Fin}/> <div className={styles.side}>Utkarsh Tripathi</div><br></br>
+                <Image className={styles.image3} src={Fin}/> <div className={styles.side}>Utkarsh Tripathi</div><br></br>
               </div>
-              <span className={styles.gen2}>(Finance Secretary)</span>
+              <span className={styles.gen}>(Finance Secretary)</span>
             </a>
             </div>
 
