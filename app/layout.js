da4385b5539a631bf94f193d62748d8dad71f09c
Jeevant Verma <jverma_be23@thapar.edu>
2024-02-10 16:59:48 +0530

    Minor responsiveness fixes

--- app/layout.js
+++ app/layout.js
@@ -1,37 +1,37 @@
-"use client"
+// "use client"
 import './globals.css'
 import { Footer } from '@/components/footer/Footer'
 import Hamburger from '@/components/Hamburger/Hamburger'
 import { usePathname } from 'next/navigation'
 
-// export const metadata = {
-//   title: 'Makeathon 6',
-//   description: 'The official Makeathon website',
-// }
+export const metadata = {
+  title: 'Makeathon 6',
+  description: 'The official Makeathon website',
+}
 
 export default function RootLayout({ children }) {
-  const pathName = usePathname();
-  if (pathName == "/") {
+  // const pathName = usePathname();
+  // if (pathName == "/") {
     return (
       <html lang="en">
         <body>{children}</body>
       </html>
     );
-  } else if(pathName=='/timeline'){
-    return ( 
-      <html lang="en">
-        <Hamburger/>
-        <body>{children}</body>
-      </html>
-    );
-    }else{
-      return(
-      <html lang="en">
-        <Hamburger/>
-        <body>{children}</body>
-        <Footer/>
-      </html>
-      );
-  }
+  // } else if(pathName=='/timeline'){
+  //   return ( 
+  //     <html lang="en">
+  //       <Hamburger/>
+  //       <body>{children}</body>
+  //     </html>
+  //   );
+  //   }else{
+  //     return(
+  //     <html lang="en">
+  //       <Hamburger/>
+  //       <body>{children}</body>
+  //       <Footer/>
+  //     </html>
+  //     );
+  // }
 }
 
