657453dbb2b13c5dc7b21672644a04d0e6ec44d1
Daanish Mittal <daanishmittal24@gmail.com>
2024-02-13 18:38:17 +0530

    fixed css

--- app/about/components/Faq.jsx
+++ app/about/components/Faq.jsx
@@ -29,25 +29,24 @@ const faqData = [
   //   answer:
   //     'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
   // },
-  
+
 ];
 
 export default function FAQ() {
   return (
     <div style={{ width: '90%', margin: '0 auto' }}>
-      <h2>Frequently Asked Questions</h2>
-      {faqData.map((item, index) => (
-        <Accordion 
-          key={index} 
-          sx={{ 
-            backgroundColor: 'rgba(255, 255, 255, 0.1)', 
+      <div style={{ padding: '10 1em' }}>{faqData.map((item, index) => (
+        <Accordion
+          key={index}
+          sx={{
+            backgroundColor: 'rgba(255, 255, 255, 0.1)',
             border: '2px solid rgba(255, 255, 255, 0.8)',
             marginBottom: '10px',
             borderRadius: '10px'
           }}
         >
           <AccordionSummary
-            expandIcon={<ExpandMoreIcon sx={{ color: '#ffffff' }} />}
+            expandIcon={<ExpandMoreIcon sx={{ color: '#ffffff', padding: '0 1em' }} />}
             aria-controls={`panel${index + 1}a-content`}
             id={`panel${index + 1}a-header`}
             sx={{ color: '#ffffff', padding: '1em 0' }}
@@ -55,10 +54,11 @@ export default function FAQ() {
             <Typography sx={{ fontSize: '18px', padding: '0 1em' }}>{item.question}</Typography>
           </AccordionSummary>
           <AccordionDetails sx={{ color: '#ffffff', padding: '1em 2em' }}>
-            <Typography sx={{ fontSize: '16px' }}>{item.answer}</Typography>
-          </AccordionDetails>
+            <Typography sx={{ fontSize: '16px', padding: '0 1em' }}>{item.answer}</Typography>
+          </AccordionDetails >
         </Accordion>
       ))}
+      </div>
     </div>
   );
 }
