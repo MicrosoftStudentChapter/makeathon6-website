a85754e1aa2f72a044a339da4ebcb7b9de260625
Mankirat Singh <msingh2_be22@thapar.edu>
2024-02-21 21:33:23 +0530

    i updated the font :D

--- components/font/myfont.js
+++ components/font/myfont.js
@@ -1,8 +1,22 @@
 import localFont from 'next/font/local'
+import { Jura } from 'next/font/google'
 
 // Font files can be colocated inside of `app`
-export const myFont = localFont({
-  src: '../../public/fonts/Kingvoon.woff',
+export const myFont = Jura({
   display: 'swap',
+  subsets: ['latin'],
+  weight: '500'
 })
 
+// my font bold
+export const myFontBold = Jura({
+  display: 'swap',
+  subsets: ['latin'],
+  weight: '600'
+})
+// localFont({
+//   src: '../../public/fonts/Kingvoon.woff',
+//   display: 'swap',
+// })
+
+// export const juraFont = Jura
